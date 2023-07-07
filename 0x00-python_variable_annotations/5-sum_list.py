b10698dadffc93f8de9bb7c0c2884d1ff4e64712
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-07 07:03:51 +0100

    task4

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """sum of list of float"""
+    accumulator = 0
+    for x in input_list:
+        accumulator += x
+    return accumulator
