8d29db9e74167d823b94dda182231b8e1012fb3e
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-07 16:08:34 +0100

    task

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Complex types - string and int/float to tuple
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """Takes str for first element and int or float for the second
+        then returns a Tuple of str as first element and float or int as
+        second element
+    """
+    return (k, v)
