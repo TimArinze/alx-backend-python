cdf54c136a3a055cef1831864b92ce2e5096e8c0
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-11 20:53:54 +0100

    task2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Run time for four parallel comprehension
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures the total runtime and returns it"""
+    start_time = time.time()
+    routine = [async_comprehension() for _ in range(4)]
+    routines = await asyncio.gather(*routine)
+    end_time = time.time()
+    return end_time - start_time
