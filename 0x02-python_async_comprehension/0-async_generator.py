c1f5491774e1d9c5701418e1bc29bba619d4415b
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-11 15:55:26 +0100

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """loop 10 times each time waits for 1 second
+        yields a random number from 0 - 10
+    """
+    for _ in range(0, 10):
+        random_number = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield random_number
