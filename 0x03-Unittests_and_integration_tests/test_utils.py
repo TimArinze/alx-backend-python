8f96fbc2c05d9707798d99c6363189dcd258ec90
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-31 14:24:01 +0100

    task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,10 +57,10 @@ class TestMemoize(unittest.TestCase):
                 """a_property"""
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-            my_object = TestClass()
-            my_object.a_property
-            my_object.a_property
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_class = TestClass()
+            test_class.a_property
+            test_class.a_property
             mock_method.assert_called_once()
 
 
