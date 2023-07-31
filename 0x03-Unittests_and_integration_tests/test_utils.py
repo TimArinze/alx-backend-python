f17cf67feb3d880f0c2d01dcac8a2310c542bc70
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-31 05:46:47 +0100

    task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ Parameterize a unit test
 from parameterized import parameterized
 import unittest
 import utils
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,6 +25,22 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Testing get_json function"""
+    #test_data = [
+            #("http://example.com", {"payload": True}),
+            #]
+    # @parameterized.expand(test_data)
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = {"payload": True}
+        mock_get.return_value = mock_response
+
+        result = utils.get_json("http://example.com")
+        self.assertEqual(result, {"payload": True})
 
 
 if __name__ == "__main__":
